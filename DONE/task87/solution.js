6dbd92d8c33d29cca758ebd5aba66a1966598420
Maksimov86 <115940258+Maksimov86@users.noreply.github.com>
2024-12-04 15:19:36 +0300

    Add files

---
+++ DONE/task87/solution.js
@@ -0,0 +1,10 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+const dayOfTheWeek = 'monday';
+const monthOfTheYear = 'May';
+const year = 2024;
+const drink = 'tea';
+const age = 38;
